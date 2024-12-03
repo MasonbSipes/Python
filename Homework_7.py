d91637b10b5e9259144790a37d738973a1c990b7
Mason <96517033+MasonbSipes@users.noreply.github.com>
2024-12-03 13:29:51 -0500

    Add files via upload

---
+++ Homework_7.py
@@ -0,0 +1,9 @@
+# Homework 7
+# Task: Create a for loop that prints numbers from 1 to 5.
+
+# --- #
+
+# Answer
+numbers = ['1', '2', '3', '4', '5']
+for num in numbers: # The for loop goes through each item in the list numbers.
+    print(num)
