01019da4089fe3c1c239ba9cd95b44d05905ceed
Mason <96517033+MasonbSipes@users.noreply.github.com>
2024-12-27 14:58:14 -0500

    Delete Homework_6.py

--- Homework_6.py
+++
@@ -1,9 +0,0 @@
-# Homework 6
-# Task: Write a Python program to calculate the square of a the number 7.
-
-# --- #
-
-number = input('Enter a number to find the square: ')
-square = int(number) ** 2
-
-print(f'The square of {number} is {square}.')
